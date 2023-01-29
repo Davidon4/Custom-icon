1cd8c2c7273adfc551c8ae30b265506be28587c2
Davidon4 <Quayson450@gmail.com>
2023-01-29 04:08:47 +0100

    'added-icons'

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, Text } from "react-native";
+import * as Font from "expo-font";
 
-export default function App() {
+import { createIconSetFromIcoMoon } from "react-native-vector-icons";
+import icoMoonConfig from "./selection.json";
+const Icon = createIconSetFromIcoMoon(icoMoonConfig);
+
+const App = () => {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  useEffect(() => {
+    const loadFonts = async () => {
+      try {
+        await Font.loadAsync({
+          "roboto-mono": require("./assets/fonts/RobotoMono-Regular.ttf"),
+          "icomoon": require("./assets/fonts/icomoon.ttf"),
+        });
+        console.log("Fonts loaded successfully");
+        setFontLoaded(true);
+      } catch (error) {
+        console.log("Error loading fonts: ", error);
+      }
+    };
+    loadFonts();
+  }, []);
+  if (!fontLoaded) {
+    console.log("Fonts are still loading...");
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.container, styles.center]}>
+      <Icon name="phone" style={[styles.colorBlue, styles.fontSize30]} />
+      <Text>This is a phone</Text>
     </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#f2f2f2",
+  },
+  center: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  colorBlue: {
+    color: "#002080",
+  },
+  fontSize30: {
+    fontSize: 30,
   },
 });
+
+// console.log(
+//   `Checking if file ${"./assets/fonts/icomoon.ttf"} exists...`
+// );
+// const icomoon = require("./assets/fonts/icomoon.ttf");
+// console.log(`File ${"./assets/fonts/icomoon.ttf"} exists.`);
